5e62117e8f12dc4c7df5e6c2346f8696ed4ff261
Gael AHOUANVOEDO <97810968+gaelahouanvoedo@users.noreply.github.com>
2024-03-19 13:56:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if menu == "Lancer l'app":
                 st.info(f"Il y a {len(df_05)} CVs avec une similarité supérieure à 0.5.")
             if len(df_07) > 0:
                 st.success(f"Il y a {len(df_07)} CVs avec une similarité supérieure à 0.7.")
-                st.markdown("**Les 10 CVs qui correspondent le mieux :**")
+                st.markdown("**Les CVs qui correspondent le mieux :**")
                 rank = 1
                 for idx, row in df_07.head(10).iterrows():
                     expander = st.expander(f"{rank} - {row['nom_fichier']} - Cliquez pour voir les compétences")
